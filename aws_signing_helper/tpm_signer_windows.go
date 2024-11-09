00ee1c2dddc67a510b0c6e102472f8d9aeed2c57
Ajay Gupta <guptagr@amazon.com>
2024-11-09 21:41:12 +0000

    Fix GetTPMv2Signer function signature in Windows file
    * Rename platform-specific files to be consistent in format

--- aws_signing_helper/tpm_signer_windows.go
+++ aws_signing_helper/tpm_signer_windows.go
@@ -6,6 +6,6 @@ import (
 	"errors"
 )
 
-func GetTPMv2Signer(certIdentifier CertIdentifier) (signer Signer, signingAlgorithm string, err error) {
+func GetTPMv2Signer(opts GetTPMv2SignerOpts) (signer Signer, signingAlgorithm string, err error) {
 	return nil, "", errors.New("unable to use tpm v2 signer on windows")
 }
