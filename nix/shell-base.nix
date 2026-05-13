efebb0567a7e28798ce9fe7f743b442d09660e9d
Cameron Dugan <camdugan@amazon.com>
2026-05-13 09:25:38 -0400

    ci(nix): pin nixpkgs to commit with Go 1.26.2
    Pin all nix shells to nixpkgs 01fbdeef which provides Go 1.26.2,
    satisfying the go.mod minimum version requirement.
    
    To update: find a nixpkgs commit with the desired Go version at
    https://www.nixhub.io/packages/go then replace the commit hash.

--- nix/shell-base.nix
+++ nix/shell-base.nix
@@ -1,4 +1,9 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (builtins.fetchTarball {
+    # nixpkgs with Go 1.26.2
+    # To update: find a nixpkgs commit with the desired Go version at
+    # https://www.nixhub.io/packages/go then replace the commit hash below.
+    url = "https://github.com/NixOS/nixpkgs/archive/01fbdeef22b76df85ea168fbfe1bfd9e63681b30.tar.gz";
+  }) {} }:
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
